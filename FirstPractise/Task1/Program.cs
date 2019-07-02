ff8c25edc782d810c5db4652e15326987c2cc169
Denis Samusevich <samynchik@gmail.com>
2019-07-02 20:26:19 +0300

    Task01

---
+++ FirstPractise/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short sht = 10;
+            object o = (object)sht;
+          
+
+            System.SByte shrt2 = (System.SByte)o;
+            System.Console.WriteLine(shrt.GetType());
+        }
+    }
+}
