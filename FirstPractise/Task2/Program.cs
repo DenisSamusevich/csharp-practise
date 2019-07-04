6383c6670f38498b5b855357ea9913eb0ab99fd7
Denis Samusevich <samynchik@gmail.com>
2019-07-04 21:21:32 +0300

    added Task2

---
+++ FirstPractise/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList arrayList = new System.Collections.ArrayList();
+
+            arrayList.Add(45); //true, boxing
+            arrayList.Add(true); //true, boxing
+            arrayList.Add("Hello!"); //false
+            arrayList.Add(23.45); //true, boxing
+        }
+    }
+}
