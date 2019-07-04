9cde1267c0877bb8e6a7ef25e95efd32768848c3
Denis Samusevich <samynchik@gmail.com>
2019-07-04 21:47:33 +0300

    added Task1

---
+++ SecondPractise/SecondPractise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            if (x => 9 & x <= 12)
+                Console.WriteLine("Good morning, guys");
+            else if (x => 12 & x <= 15)
+                Console.WriteLine("Good day, guys");
+            else if (x => 15 & x <= 22)
+                Console.WriteLine("Good day, guys");
+            else;
+        }
+    }
+}
