37b9371a4c30c84006b3b665b98fcd2b90181490
Denis Samusevich <samynchik@gmail.com>
2019-07-09 20:54:14 +0300

    added 3 prac

---
+++ for_letters_count/for_letters_count/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_letters_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите английское слово");
+            string world = Console.ReadLine();
+            int s = 0;
+            for (int i = 0; i < world.Length; i++)
+            {
+                if (world[i] == "a")
+                    s += 1;
+         
+            }
+            if (s == 0)
+                Console.WriteLine("В данном слове нет букв 'a'");
+            Console.WriteLine("В данном слове " + s + " букв 'a'");
+        }
+    }
+}
