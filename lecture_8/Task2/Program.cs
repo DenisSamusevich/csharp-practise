9ff8a26e35653929192e40abe6db47969e4f39e0
Denis Samusevich <samynchik@gmail.com>
2019-07-16 20:03:46 +0300

    task2

---
+++ lecture_8/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] massiv = new object[3];
+            int num32 = 32;
+            char A = 'A';
+            string hello = "Hello";
+            massiv[0] = num32;
+            massiv[1] = A;
+            massiv[2] = hello;
+            Console.WriteLine(massiv[0]);
+            Console.WriteLine(massiv[1]);
+            Console.WriteLine(massiv[2]);
+            Console.ReadLine();
+
+
+        }
+    }
+}
