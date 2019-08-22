c17ed7fcf0f1b726b93d11208fcdfd4e95389ad2
Denis Samusevich <samynchik@gmail.com>
2019-08-22 21:03:02 +0300

    Done

---
+++ lecture_15/lecture_15/stringExten.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_15
+{
+    static class stringExten
+    {
+        public static string StringfiveSymbol(this string stringUpdate)
+        {
+            string stringReturn = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                stringReturn += stringUpdate[i];
+            }
+            stringReturn = stringReturn + "...";
+            return stringReturn;
+        }
+    }
+}
